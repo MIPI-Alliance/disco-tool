131f7efb55e75139bbff5b261e2cc18555f18ad1
Saurin Shah (OSG) <sashah@microsoft.com>
2021-03-23 17:22:05 -0700

    Updated Copyright information.

--- Source/test.py
+++ Source/test.py
@@ -1,6 +1,7 @@
 #  ------------------------------------------------------------------------------
 #
-#  Copyright (C) Microsoft Corporation. All rights reserved.
+#  Copyright 2021, MIPI Alliance and its contributors.
+#  SPDX-License-Identifier: BSD-3-Clause
 #
 #  Module Name:
 #

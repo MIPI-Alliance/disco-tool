44c69ffab45309d17593ff544c873b3187ff3fd4
Purma <kondal.r.purma@intel.com>
2022-02-02 13:47:24 -0800

    Making DisCo as pakage with entry point

---
+++ src/disco/__init__.py
@@ -0,0 +1,22 @@
+#  ------------------------------------------------------------------------------
+#
+#  Copyright 2022, MIPI Alliance and its contributors.
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  Module Name:
+#
+#    init.py
+#
+#  Abstract:
+#
+#    Disco pakage entry point.
+#
+#  -------------------------------------------------------------------------------
+
+from   disco.controller import  controller
+import wx
+def main():
+    """Entry point for the application script"""
+    app = wx.App()
+    myController = controller()
+    app.MainLoop()	

8c3024fcef1f4d5f71bc19f1decc66dc25d42b1c
Keith <keithcarriere3@gmail.com>
2025-04-23 16:15:16 -0400

    [Issue 78] Package name warning message when renaming hierarchical property (#100)
    * formatting
    
    * adjust warning message and dialog options
    
    * pythonic boolean check
    
    * empty test commit
    
    * Test commit
    
    * Revert "Test commit"
    
    This reverts commit 92359f361ff0fd5b8a8f60214f5021a50f0b93bd.
    
    ---------
    
    Co-authored-by: saurinshah9 <38705196+saurinshah9@users.noreply.github.com>
    Co-authored-by: Saurin Shah <sashah@microsoft.com>

--- src/disco/hierarchical_properties.py
+++ src/disco/hierarchical_properties.py
@@ -276,7 +276,7 @@ class HierarchicalPropertyPanel(wx.Panel):
 
         # initializes variables to starting values
         over_four = False
-        ancestor = False
+        is_ancestor = False
         result_new = wx.ID_NONE
         result_old = wx.ID_NONE
 
@@ -295,8 +295,8 @@ class HierarchicalPropertyPanel(wx.Panel):
                 # the over_four variable is set to true
                 if len(value) > 4:
                     wx.MessageBox(message=value + disco_str.DISCO_STR_GRIDCELL_MAXCHAR_MSG,
-                                  caption='Package name error',
-                                  style=wx.OK | wx.ICON_ERROR)
+                                caption='Package name error',
+                                style=wx.OK | wx.ICON_ERROR)
                     over_four = True
 
                 # iterates through the current element tree list to see if the new value already has an associated
@@ -311,16 +311,24 @@ class HierarchicalPropertyPanel(wx.Panel):
                     # and send a warning message telling the user
                     # if they use this value, they will be sharing this package among other parents.
                     if tree.getroot().find('Name').text == value:
-                        ancestor = self.is_ancestor(tree, self.main_window.curr_tree)
+                        is_ancestor = self.is_ancestor(tree, self.main_window.curr_tree)
 
-                        if ancestor is True:
-                            wx.MessageBox(message=disco_str.DISCO_STR_GRIDCELL_DUPLICATE_MSG,
-                                          caption='Package name error',
-                                          style=wx.OK | wx.ICON_ERROR)
+                        if is_ancestor:
+                            wx.MessageBox(
+                                message=disco_str.DISCO_STR_GRIDCELL_DUPLICATE_MSG,
+                                caption='Package name error',
+                                style=wx.OK | wx.ICON_ERROR,
+                            )
                         else:
-                            result_new = wx.MessageBox(message=value + disco_str.DISCO_STR_GRIDCELL_REUSE_MSG,
-                                                       caption='Package name warning',
-                                                       style=wx.YES_NO | wx.ICON_WARNING)
+                            dialog = wx.MessageDialog(
+                                self,
+                                message=value + disco_str.DISCO_STR_GRIDCELL_REUSE_MSG,
+                                caption='Package name warning',
+                                style=wx.YES_NO | wx.ICON_WARNING,
+                            )
+                            dialog.SetYesNoLabels("Re-use existing", "Cancel rename")
+                            result_new = dialog.ShowModal()
+                            dialog.Destroy()
 
                     # Condition when the element tree is found that was associated with the previous value.
                     # Counter variable is used to count how many parents this tree has -
@@ -333,14 +341,14 @@ class HierarchicalPropertyPanel(wx.Panel):
 
                         if counter > 1:
                             result_old = wx.MessageBox(message=old_value + disco_str.DISCO_STR_GRIDCELL_OLD_SHARED_MSG,
-                                                       caption='Package name warning',
-                                                       style=wx.YES_NO | wx.ICON_WARNING)
+                                                    caption='Package name warning',
+                                                    style=wx.YES_NO | wx.ICON_WARNING)
 
                 # if the new value is not an ancestor of the current tree, has equal or less than four characters,
                 # and the user did not reply "no" to any warning messages they might have gotten,
                 # then the hierarchical property is changed in the Model. Otherwise, the property
                 # is not changed in the model and the grid cell value returns to its previous value.
-                if (ancestor is False) & (over_four is False) & (result_new != wx.NO) & (result_old != wx.NO):
+                if (not is_ancestor) & (not over_four) & (result_new != wx.ID_NO) & (result_old != wx.NO):
                     # Print statement for debugging purposes:
                     print("publishing property " + value)
 
